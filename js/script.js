3f174fb3b55b9e20e694a7409db9a23b242e27d3
JOFILATECH <44501079+jofilacode@users.noreply.github.com>
2025-03-20 09:00:37 +0100

    Assets/Resources

---
+++ js/script.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+
+    $(".buy").click(function (evt) {
+      
+        evt.preventDefault();
+        window.location.replace("product.aspx");
+    });
+
+    $(".personal img").click(function () {
+
+        $(".dropbox").slideToggle();
+
+    });
+
+    setTimeout(fade_out, 6000);
+
+    function fade_out() {
+        $(".msg").fadeOut(1000).empty();
+
+    }
+
+    });
